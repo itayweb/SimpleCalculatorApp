4b6b84a5aed2d3972aca40ea2fabe2b3d2d2a57e
unknown <itayadler13@gmail.com>
2020-11-04 13:46:29 +0200

    Initial Commit

---
+++ SimpleCalculatorApp/MainWindow.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace SimpleCalculatorApp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainLbl = new System.Windows.Forms.Label();
+            this.divider = new System.Windows.Forms.Label();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnDecimal = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnEntr = new System.Windows.Forms.Button();
+            this.btnDivison = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnClr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mainLbl
+            // 
+            this.mainLbl.AutoSize = true;
+            this.mainLbl.Font = new System.Drawing.Font("Cambria Math", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mainLbl.Location = new System.Drawing.Point(12, 9);
+            this.mainLbl.MaximumSize = new System.Drawing.Size(10000, 100000);
+            this.mainLbl.MinimumSize = new System.Drawing.Size(300, 75);
+            this.mainLbl.Name = "mainLbl";
+            this.mainLbl.Size = new System.Drawing.Size(300, 195);
+            this.mainLbl.TabIndex = 0;
+            this.mainLbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // divider
+            // 
+            this.divider.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.divider.Location = new System.Drawing.Point(12, 229);
+            this.divider.MaximumSize = new System.Drawing.Size(0, 2);
+            this.divider.MinimumSize = new System.Drawing.Size(325, 2);
+            this.divider.Name = "divider";
+            this.divider.Size = new System.Drawing.Size(325, 2);
+            this.divider.TabIndex = 1;
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn7.Location = new System.Drawing.Point(12, 327);
+            this.btn7.Margin = new System.Windows.Forms.Padding(0);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 51);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn8.Location = new System.Drawing.Point(96, 327);
+            this.btn8.Margin = new System.Windows.Forms.Padding(0);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 51);
+            this.btn8.TabIndex = 0;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn9.Location = new System.Drawing.Point(178, 327);
+            this.btn9.Margin = new System.Windows.Forms.Padding(0);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 51);
+            this.btn9.TabIndex = 0;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn4.Location = new System.Drawing.Point(12, 388);
+            this.btn4.Margin = new System.Windows.Forms.Padding(0);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 51);
+            this.btn4.TabIndex = 0;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn5.Location = new System.Drawing.Point(96, 388);
+            this.btn5.Margin = new System.Windows.Forms.Padding(0);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 51);
+            this.btn5.TabIndex = 0;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn6.Location = new System.Drawing.Point(178, 388);
+            this.btn6.Margin = new System.Windows.Forms.Padding(0);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 51);
+            this.btn6.TabIndex = 0;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(12, 449);
+            this.btn1.Margin = new System.Windows.Forms.Padding(0);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 51);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(96, 449);
+            this.btn2.Margin = new System.Windows.Forms.Padding(0);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 51);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(178, 449);
+            this.btn3.Margin = new System.Windows.Forms.Padding(0);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 51);
+            this.btn3.TabIndex = 0;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn0.Location = new System.Drawing.Point(12, 509);
+            this.btn0.Margin = new System.Windows.Forms.Padding(0);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(159, 51);
+            this.btn0.TabIndex = 0;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnDecimal
+            // 
+            this.btnDecimal.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDecimal.Location = new System.Drawing.Point(178, 509);
+            this.btnDecimal.Margin = new System.Windows.Forms.Padding(0);
+            this.btnDecimal.Name = "btnDecimal";
+            this.btnDecimal.Size = new System.Drawing.Size(75, 51);
+            this.btnDecimal.TabIndex = 0;
+            this.btnDecimal.Text = ".";
+            this.btnDecimal.UseVisualStyleBackColor = true;
+            this.btnDecimal.Click += new System.EventHandler(this.btnDecimal_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMinus.Location = new System.Drawing.Point(261, 267);
+            this.btnMinus.Margin = new System.Windows.Forms.Padding(0);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(75, 51);
+            this.btnMinus.TabIndex = 0;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPlus.Location = new System.Drawing.Point(261, 327);
+            this.btnPlus.Margin = new System.Windows.Forms.Padding(0);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 112);
+            this.btnPlus.TabIndex = 0;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnEntr
+            // 
+            this.btnEntr.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEntr.Location = new System.Drawing.Point(261, 449);
+            this.btnEntr.Margin = new System.Windows.Forms.Padding(0);
+            this.btnEntr.Name = "btnEntr";
+            this.btnEntr.Size = new System.Drawing.Size(75, 111);
+            this.btnEntr.TabIndex = 0;
+            this.btnEntr.Text = "Enter";
+            this.btnEntr.UseVisualStyleBackColor = true;
+            this.btnEntr.Click += new System.EventHandler(this.btnEntr_Click);
+            // 
+            // btnDivison
+            // 
+            this.btnDivison.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDivison.Location = new System.Drawing.Point(96, 267);
+            this.btnDivison.Margin = new System.Windows.Forms.Padding(0);
+            this.btnDivison.Name = "btnDivison";
+            this.btnDivison.Size = new System.Drawing.Size(75, 51);
+            this.btnDivison.TabIndex = 0;
+            this.btnDivison.Text = "/";
+            this.btnDivison.UseVisualStyleBackColor = true;
+            this.btnDivison.Click += new System.EventHandler(this.btnDivison_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMul.Location = new System.Drawing.Point(178, 267);
+            this.btnMul.Margin = new System.Windows.Forms.Padding(0);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(75, 51);
+            this.btnMul.TabIndex = 0;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnClr
+            // 
+            this.btnClr.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClr.Location = new System.Drawing.Point(12, 267);
+            this.btnClr.Margin = new System.Windows.Forms.Padding(0);
+            this.btnClr.Name = "btnClr";
+            this.btnClr.Size = new System.Drawing.Size(75, 51);
+            this.btnClr.TabIndex = 0;
+            this.btnClr.Text = "CLR";
+            this.btnClr.UseVisualStyleBackColor = true;
+            this.btnClr.Click += new System.EventHandler(this.btnClr_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 569);
+            this.Controls.Add(this.btnClr);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnDivison);
+            this.Controls.Add(this.btnEntr);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnDecimal);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.divider);
+            this.Controls.Add(this.mainLbl);
+            this.MaximumSize = new System.Drawing.Size(368, 608);
+            this.MinimumSize = new System.Drawing.Size(368, 608);
+            this.Name = "MainWindow";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mainLbl;
+        private System.Windows.Forms.Label divider;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnDecimal;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnEntr;
+        private System.Windows.Forms.Button btnDivison;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnClr;
+    }
+}
+
---
+++ SimpleCalculatorApp/MainWindow.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculatorApp
+{
+    public partial class MainWindow : Form
+    {
+        double calc1, calc2;
+        char symb;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClr_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text = "";
+        }
+
+        private void btnDivison_Click(object sender, EventArgs e)
+        {
+            calc1 = Convert.ToDouble(mainLbl.Text);
+            mainLbl.Text = "";
+            symb = '/';
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += "9";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += "0";
+        }
+
+        private void btnDecimal_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += ".";
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            calc1 = Convert.ToDouble(mainLbl.Text);
+            mainLbl.Text = "";
+            symb = '*';
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            mainLbl.Text += "1";
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            calc1 = Convert.ToDouble(mainLbl.Text);
+            mainLbl.Text = "";
+            symb = '-';
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            calc1 = Convert.ToDouble(mainLbl.Text);
+            mainLbl.Text = "";
+            symb = '+';
+        }
+
+        private void btnEntr_Click(object sender, EventArgs e)
+        {
+            calc2 = Convert.ToDouble(mainLbl.Text);
+
+            switch (symb)
+            {
+                case '+':
+                    mainLbl.Text = (calc1 + calc2).ToString();
+                    break;
+                case '-':
+                    mainLbl.Text = (calc1 - calc2).ToString();
+                    break;
+                case '*':
+                    mainLbl.Text = (calc1 * calc2).ToString();
+                    break;
+                case '/':
+                    mainLbl.Text = (calc1 / calc2).ToString();
+                    break;
+            }
+        }
+    }
+}
